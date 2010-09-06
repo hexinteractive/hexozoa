8b7047c1b9835f613b5993933c1ed4cbaa6d77b2
Bryan Yurasits <comcast@DIVWL-52G9YJZA.(none)>
2010-09-06 10:15:45 -0400

    added simple collision detection with the walls, but they then get stuck near the walls

--- jcode.js
+++ jcode.js
@@ -9,15 +9,9 @@ parseInt(hex,16);
 */
 
 
-
-
 $(document).ready(function(){
 
 //setInterval((function(){$('body').trigger('tick')}),500);
-
-function randomHexgene() {
-	return '#'+ (parseInt(Math.random() * 255, 10)).toString(16) + (parseInt(Math.random() * 255, 10)).toString(16) + (parseInt(Math.random() * 255, 10)).toString(16);
-}
 
 function Hexozoa(hexgenes) {
 	this.hexgenes = hexgenes.split('#').slice(1);//split the hexgenes into an array of hexcodes without the # and removes the first item which is empty.
@@ -28,42 +22,62 @@ function Hexozoa(hexgenes) {
 	this.element = (function(self) {
 		return ($('<i id="'+ this.id +'" class="hexozoa" style="background-color:#'+ this.color +';"></i>').css({
 			'background-color': function(){return '#'+self.color;},
-			'top': function(){return parseInt(Math.random() * $('#box').height()) },
-			'left': function(){return parseInt(Math.random() * $('#box').width()) }
+			'top': function(){return parseInt(Math.random() * bioreactor.height) },
+			'left': function(){return parseInt(Math.random() * bioreactor.width) }
 		})).appendTo('#box');
 	})(this);
 }
 
 Hexozoa.prototype.swim = function(radian){
 	
-	var info = this.interpret(this.hexgenes[1]);
+	var top = parseInt($(this.element).css('top'));
+	var left = parseInt($(this.element).css('left'));
+	
+	var info = this.interpret(this.hexgenes[1]);//interpreting on each swim call is really inefficient 
 	var dur = (Math.random() * 200) + info.g;
 	var dist = parseInt(info.b/25,10);
 	var self = this;
 	
-	
-	
 	//var alpha = -45 * (Math.PI/180);
 	//if a radian was passed into the swim func then use it else generate a random angle (6.2rad is the complete circle)
-	radian = typeof radian == 'number' ? radian : (Math.random() * 6.2);
+	radian = (typeof radian == 'number') ? radian : (Math.random() * 6.2);
 	//define alpha and update the value of radian to pass to the swim func on completion
 	//subtract .5 from random to allow L & R turns  
-	var alpha = radian = radian + (Math.random() - .5);
+	
+	
+	var alpha = (radian = radian + (Math.random() - .5));
 		
 		/*
 		//random (0-1) plus RED (0-255) divided by 255 divided by 2 (that makes the value range 0-1) minus .5 (which makes the value range -0.5 to 0.5)
 		var alpha = radian = radian + ( ( (Math.random() + ( info.r / 255 ) ) / 2 ) - .5);
-		if(this === window.creatures[6]){console.log('delta radian: ', ( ( (Math.random() + ( info.r / 255 ) ) / 2 ) - .5))}
+		if(this === bioreactor.creatures[6]){console.log('delta radian: ', ( ( (Math.random() + ( info.r / 255 ) ) / 2 ) - .5))}
 		*/
-			//var alpha = radian = radian + ( (Math.random() - .5)     ( info.r / 255 ) ) / 2 )      );
-		
-
-	var hypotenuse = dist;
-	var a = Math.sin(alpha) * hypotenuse;
-	var b = Math.cos(alpha) * hypotenuse;
+		//var alpha = radian = radian + ( (Math.random() - .5)     ( info.r / 255 ) ) / 2 )      );
 	
-	var top = parseInt($(this.element).css('top'));
-	var left = parseInt($(this.element).css('left'));
+	
+	//ra_2_de = value * (180 / pi);
+	//de_2_ra = value * (pi / 180);
+		
+	var minEdge = dist;
+	var minPos = Math.min(Math.min(top,left), minEdge);
+	if( minPos != minEdge) {//find the lowest coord value then find the lowest value between the lowest coord and minEdge if outcome isnt equal to minEdge then 
+		dist = 1;
+		alpha = (radian = radian + ((Math.random() - 0.5) * 1.5) );
+	}
+
+	var maxEdge = bioreactor.width - dist;//assuming height & width are always equal I used width
+	if( Math.max(maxEdge, Math.max(top,left)) != maxEdge) {
+		dist = 1;
+		alpha = (radian = radian + ((Math.random() - 0.5) * 1.5) );
+	}
+	
+	
+	
+
+		
+	var hypotenuse = dist;
+	var a = Math.sin(alpha) * hypotenuse;/*top*/
+	var b = Math.cos(alpha) * hypotenuse;/*left*/
 	
 	/*
 	var triangle = {
@@ -108,13 +122,14 @@ window.bioreactor = {
 			return '#'+ (parseInt(Math.random() * 255, 10)).toString(16) + (parseInt(Math.random() * 255, 10)).toString(16) + (parseInt(Math.random() * 255, 10)).toString(16);
 	},
 	genesis: function(seedSize) {
+		seedSize = (typeof seedSize == 'number') ? seedSize : 20;
 		seedSize = bioreactor.creatures.length + seedSize + 1;
 		for(var i=bioreactor.creatures.length; i<seedSize; i++) {
 
 			var numberOfGenes = 2;
 			var hexgenes = [];
 			for(var hG=0; hG<=1; hG++) {
-				hexgenes[hG] = randomHexgene();
+				hexgenes[hG] = bioreactor.randomHexgene();
 			}
 			bioreactor.creatures[i] = new Hexozoa(hexgenes.join(''));
 			bioreactor.creatures[i].swim();
@@ -135,39 +150,22 @@ window.bioreactor = {
 	
 };
 
-// window.creatures = [];
-// window.genesis = function genesis(seedSize) {
-// 	seedSize = window.creatures.length + seedSize + 1;
-// 	for(var i=window.creatures.length; i<seedSize; i++) {
-// 		
-// 		var numberOfGenes = 2;
-// 		var hexgenes = [];
-// 		for(var hG=0; hG<=1; hG++) {
-// 			hexgenes[hG] = randomHexgene();
-// 		}
-// 		window.creatures[i] = new Hexozoa(hexgenes.join(''));
-// 		window.creatures[i].swim();
-// 	}
-// } 
-// genesis(20);
+	//LET THERE BE LIFE
+	bioreactor.genesis();
 
-// window.ele = function ele () { /* Extinction-Level Event */
-// 	$(window.creatures).each(function(index){
-// 		window.creatures[index].element.addClass('kill');
-// 		delete window.creatures[index].element;
-// 	});
-// 	
-// 	$('#box').addClass('boom');
-// 	setTimeout(function(){$('.kill').remove()},100);
-// 	setTimeout(function(){$('#box').removeClass('boom')},100);
-// 	window.creatures = [];
-// }
-
-
-
-
-	bioreactor.genesis(20);
-	$('#btn_genesis').click(function(){bioreactor.genesis(20)});
+//interface
+	$('#btn_genesis').click(function(){bioreactor.genesis()});
+	$(document).keydown(function(e){
+		if(e.which == 71) {
+			bioreactor.genesis();
+		}
+	});
+	
 	$('#btn_ele').click(bioreactor.ele);
+	$(document).keydown(function(e){
+		if(e.which == 69) {
+			bioreactor.ele();
+		}
+	});
 
 });
